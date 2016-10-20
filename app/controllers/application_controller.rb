2898d826a95f9167b65420fbd2d6247e2d6609de
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-10-20 10:24:24 +0700

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+  	render html:"hello, world!"
+  end
+
+ def goobye
+ 	render html:"goobye, see u again!"
+ end
+
+end
